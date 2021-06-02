28288444252397cffbd12ffb815397fcf33174cf
Ray Messina <rmessina10@gmail.com>
2021-06-02 00:28:52 -0500

    Basic Clock functions

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <DigitalClock acc={3} />
+        <DigitalClock acc={6} mer={true} mil={true} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/components/clock.js
+++ src/components/clock.js
@@ -11,33 +11,46 @@ class DigitalClock extends Component {
             { clss: '-hr', int: 3600000, foo: (time, mil) => mil ? time.getHours().toString().padStart(2, '0') : time.getHours() % 12 || 12 },
             { clss: '-min', int: 60000, foo: (time) => time.getMinutes().toString().padStart(2, '0'), sep: ':' },
             { clss: '-sec', int: 1000, foo: (time) => time.getSeconds().toString().padStart(2, '0'), sep: ':' },
-            { clss: '-mil', int: 10 ** (6 - this.props.acc), foo: (time) => Math.round(time.getMilliseconds() / 10 ** (6 - this.props.acc)).toString().padStart(2, '0'), sep: '.' },
+            {
+                clss: '-mil', int: 10 ** (6 - this.props.acc), foo: (time) => {
+                    let tens = 10 ** (6 - this.props.acc);
+                    let places = this.props.acc - 3;
+                    return Math.round(time.getMilliseconds() / tens).toString().padStart(places, '0').substr(0, tens + 1)
+                },
+                sep: '.'
+            },
         ];
         this.int = this.props
         this.state = this.timeObj(this.startTime, this.int);
     }
 
+    componentDidMount() {
+        setInterval(this.tic, this.state.int)
+    }
+
     timeObj = (time) => {
+        let acc = (this.props.acc > -1 && this.props.acc < 7) ? this.props.acc : 2;
+        let rng = acc > 4 ? 4 : acc;
         return {
             time,
-            int: (this.props.lev > -1 && this.props.lev < 7) ? this.depths[this.props.lev].int : 2,
+            int: this.depths[rng].int,
             isMil: this.props.mil ? true : false,
             showMer: this.props.mer ? true : false,
             mer: this.props.mil ? 'hours' : time.getHours() > 11 ? 'am' : 'pm',
-            acc: (this.props.acc > -1 && this.props.acc < 6) ? this.props.acc : 2,
+            acc,
             showDate: this.props.date ? true : false
         }
     }
 
 
     tic = () => {
-
+        this.setState(state => ({ time: new Date(state.time.getTime() + state.int) }));
     }
 
     render() {
         let parts = [];
         console.log(this.state.time)
-        for (let i = this.state.showDate ? 0 : 1; i <= this.state.acc; i++) {
+        for (let i = this.state.showDate ? 0 : 1, rng = this.state.acc < 4 ? this.state.acc : 4; i <= rng; i++) {
             let dobj = this.depths[i];
             parts.push(<span key={'clock' + dobj.class} className={'clock' + dobj.class}>{(dobj.sep || '') + dobj.foo(this.state.time, this.state.isMil)}</span>)
         }
