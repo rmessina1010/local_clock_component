8b02ef6ec4174a656545e882cd80a0443ac166c4
Ray Messina <rmessina10@gmail.com>
2021-06-04 12:44:10 -0500

    Adds show date functionality

--- src/components/clock.js
+++ src/components/clock.js
@@ -8,12 +8,12 @@ class DigitalClock extends Component {
         this.theTic = null;
 
         this.depths = [
-            { clss: '-day', int: 86400000 },
-            { clss: '-hr', int: 3600000, foo: (time, mil) => mil ? time.getHours().toString().padStart(2, '0') : time.getHours() % 12 || 12 },
-            { clss: '-min', int: 60000, foo: (time) => time.getMinutes().toString().padStart(2, '0'), sep: this.props.minsep || ':' },
-            { clss: '-sec', int: 1000, foo: (time) => time.getSeconds().toString().padStart(2, '0'), sep: this.props.secsep || ':' },
+            { class: '-day', int: 86400000, foo: (time, opts) => time.toLocaleDateString(opts.mode || 'en-US', opts) },
+            { class: '-hr', int: 3600000, foo: (time, mil) => mil ? time.getHours().toString().padStart(2, '0') : time.getHours() % 12 || 12 },
+            { class: '-min', int: 60000, foo: (time) => time.getMinutes().toString().padStart(2, '0'), sep: this.props.minsep || ':' },
+            { class: '-sec', int: 1000, foo: (time) => time.getSeconds().toString().padStart(2, '0'), sep: this.props.secsep || ':' },
             {
-                clss: '-mil', int: 10 ** (6 - this.props.acc), foo: (time) => {
+                class: '-mil', int: 10 ** (6 - this.props.acc), foo: (time) => {
                     return time.getMilliseconds().toString().padStart(3, '0').substr(0, this.props.acc - 3);
                 },
                 sep: this.props.milsep || '.'
@@ -58,7 +58,10 @@ class DigitalClock extends Component {
         console.log(this.state.time)
         for (let i = this.props.date ? 0 : 1, rng = this.state.acc < 4 ? this.state.acc : 4; i <= rng; i++) {
             let dobj = this.depths[i];
-            parts.push(<span key={'clock' + dobj.class} className={'clock' + dobj.class}>{(dobj.sep || '') + dobj.foo(this.state.time, this.props.mil)}</span>)
+            let aux = null;
+            if (dobj.class === '-hr') { aux = this.props.mil || null; }
+            if (dobj.class === '-day') { aux = this.props.date; }
+            parts.push(<span key={'clock' + dobj.class} className={'clock' + dobj.class}>{(dobj.sep || '') + dobj.foo(this.state.time, aux)}</span>)
         }
         if (this.props.mer) {
             parts.push((<span key='clock-mer' className='clock-mer'> {this.props.mil ? 'hours' : this.state.time.getHours() > 11 ? 'pm' : 'am'}</span>))
--- src/components/clockWrap.js
+++ src/components/clockWrap.js
@@ -8,6 +8,7 @@ class ClockWrap extends Component {
         this.state = {
             mer: true,
             mil: false,
+            date: { weekday: 'long', day: '2-digit', month: 'long' }
         }
 
     }
@@ -17,12 +18,18 @@ class ClockWrap extends Component {
             return { [e.target.name]: !oldState[e.target.name] }
         });
     }
+    handleDate = () => {
+        this.setState(oldState => {
+            return { date: oldState.date ? false : { weekday: 'long', day: '2-digit', month: 'long', year: "numeric" } }
+        });
+    }
 
     render() {
         return (<div>
             <button name="mil" onClick={this.handleClick}>toggle mil</button>
             <button name="mer" onClick={this.handleClick}>toggle show</button>
-            <DigitalClock mer={this.state.mer} mil={this.state.mil} acc={3} />
+            <button name="date" onClick={this.handleDate}>toggle day</button>
+            <DigitalClock mer={this.state.mer} mil={this.state.mil} acc={3} date={this.state.date} />
         </div>)
     }
 
